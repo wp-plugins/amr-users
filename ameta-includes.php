c77ef02274f4b9d49538f152c7bf29fbdab0e527
anmari <anmari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-22 07:51:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/amr-users/tags/1.4.2@175861 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ameta-includes.php
+++ ameta-includes.php
@@ -2,10 +2,13 @@
 /*
 */
 define('AMETA_NAME','amr-users'); 
+if ( !defined('WP_SITEURL') )
+	define( 'WP_SITEURL', get_bloginfo('wpurl')); 
 
 if (!(defined('AMR_NL'))) {
     define('AMR_NL',"\n");
 }
+
 
 /* -------------------------------------------------------------------------------------------------------------*/	
 
