3322a24506e3effaa9520d9b465eaa5be008db44
anmari <anmari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-11 06:30:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/amr-ical-events-list/trunk@239542 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- amr-ical-events-list.php
+++ amr-ical-events-list.php
@@ -31,10 +31,10 @@ Features:
     for more details.
 */
 If (ICAL_EVENTS_DEBUG) {
- ini_set('display_errors', 1);
- ini_set('log_errors', 1);
- ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
- error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('log_errors', 1);
+// ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
+// error_reporting(E_ALL);
  }
 
 define('AMR_ICAL_VERSION', '2.7');
