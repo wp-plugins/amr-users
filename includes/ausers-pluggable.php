36d3c8451bf12a8fee81b14f6c151cc3d7d2cf88
anmari <anmari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-29 12:13:52 +0000

    is plugin active
    git-svn-id: https://plugins.svn.wordpress.org/amr-users/tags/3.4.6@578685 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/ausers-pluggable.php
+++ includes/ausers-pluggable.php
@@ -1,4 +1,9 @@
 <?php 
+if (!function_exists('is_plugin_active') ) {
+	function is_plugin_active( $plugin ) {
+		return in_array( $plugin, (array) get_option( 'active_plugins', array() ) );
+	}
+}
 /* ---------------------------------------------------------------------*/	
 if (!function_exists('amr_get_href_link')) {
 	function amr_get_href_link ($field, $v, $u, $linktype) {  
